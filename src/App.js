a46004fe7171c58a5b4a5122d119d14b5080877f
Srujana-Kedigehalli <167107107+Srujana-Kedigehalli@users.noreply.github.com>
2024-11-07 09:19:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import './App.css';
-import Welcome from './welcome';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthPages from './pages/AuthPages';
+import HomePage from './pages/HomePage';
+import ServicePage from './pages/ServicePage';
+import Dashboard from './pages/Dashboard';
+import ServiceHistory from './pages/ServiceHistory';
+import StatusTracker from './pages/StatusTracker';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-   
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<AuthPages />} />
+          <Route path="/services" element={<ServicePage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/servicehistory" element={<ServiceHistory />} />
+          <Route path='/statustracker' element={<StatusTracker/>}/>
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
