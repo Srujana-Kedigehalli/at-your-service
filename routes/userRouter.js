26358020a663eea47a6fa94d1cdeed5d29ea4a92
Srujana-Kedigehalli <167107107+Srujana-Kedigehalli@users.noreply.github.com>
2024-11-15 16:06:19 +0530

    Delete routes directory

--- routes/userRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { signup, login,getUser } = require('../controllers/userController');
-const router = express.Router();
-
-router.post('/signup', signup);
-router.post('/login', login);
-router.get('/:userId', getUser);
-module.exports = router;
-
