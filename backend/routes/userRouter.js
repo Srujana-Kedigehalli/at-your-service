8711b96dbf5ef237811b11a6e646c914f14badfa
Srujana-Kedigehalli <167107107+Srujana-Kedigehalli@users.noreply.github.com>
2024-11-19 09:52:56 +0530

    Update userRouter.js

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const { signup, login,getUser } = require('../controllers/userController');
 const router = express.Router();
+const authenticateToken = require('../middleware/auth');
+const User = require('../models/userModel');
 
+router.get('/me', authenticateToken,getUser, async (req, res) => {
+    try {
+      const user = await User.findById(req.userId);
+      if (!user) return res.status(404).json({ message: 'User not found' });
+      res.json(user);
+    } catch (error) {
+      console.error('Error fetching user:', error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  });
+  
 router.post('/signup', signup);
 router.post('/login', login);
-router.get('/:userId', getUser);
 module.exports = router;
 
